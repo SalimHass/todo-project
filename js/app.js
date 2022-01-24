fcb512aa0a8e3b5bd2581b7bf4fc46ac1d945725
SalimHass <salim.hassouneh@gmail.com>
2022-01-24 08:30:12 +0200

    task

--- js/app.js
+++ js/app.js
@@ -5,24 +5,40 @@ var gender = prompt("Enter your gender");
 
 var age = prompt("Enter your age")
 
-    if (age > 0) {
-        age = age    }
-    else {
-       alert("you have entered a zero or less as an age!") 
-    };
+if (age > 0) {
+    age = age
+}
+else {
+    alert("you have entered a zero or less as an age!")
+};
 
 var conf = confirm("Do you want to skip the welcoming message?");
-if (conf == true ) {
-    }else{
-        if (gender == "male"){
-            alert("Welcome Mr "+ name)
-        }else if (gender == "female"){
-            alert("Welcome Ms "+ name)
-        }else{
-            alert("Welcome "+ name)
-        }
-    };
+if (conf == true) {
+} else {
+    if (gender == "male") {
+        alert("Welcome Mr " + name)
+    } else if (gender == "female") {
+        alert("Welcome Ms " + name)
+    } else {
+        alert("Welcome " + name)
+    }
+};
 
 
-    
+function showQuestion(question) {
+    var answer = prompt(question);
+    if (answer == "") {
+        answer = "invalid";
+    }
+    return answer;
+}
 
+var qOne = showQuestion("Do you like pizza?");
+var qTwo = showQuestion("Do you like football?");
+var qThree = showQuestion("Do like gaming?");
+
+var answers = [qOne, qTwo, qThree];
+
+for (var i = 0; i < answers.length; i++) {
+    console.log(answers[i])
+}
